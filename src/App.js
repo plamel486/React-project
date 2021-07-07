dc4447979e00ab6cf056c8afde3dd63169bc598d
plamel <alsqja147@gmail.com>
2021-07-07 09:06:09 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import './App.css';
+import React, {useState} from 'react';
 import Hi from './component/Hi';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function App() {
-  const name = "tom"
+
+  let [name,change] = useState("tom");
   return (
+    <BrowserRouter>
+
     <div className="App">
-      <h1 style={{ color:"red", backgroundColor:"blue" }}>HELLO! {name}</h1>
+
+      <h1>안녕하세요! 오신 걸 환영 합니다.</h1>
+
+      <Switch>
+          <Route exact path="/">
+      <h1>HELLO! {name}</h1>
+      <button onClick={()=>change(name==="tom"?"jane":"tom")}>변경이닷</button>
       <Hi></Hi>
       <div></div>  
           
       <div className="box">여기는 app.js 영역입니다.</div>
+
+      <Link to="/page2">숨겨진 페이지로 이동</Link>
+          </Route>
+
+        <Route exact path="/page2">
+            <h1>숨겨진 페이지!</h1>
+            <Link to="/">홈으로 이동</Link>
+
+        </Route>
+
+      </Switch>
+
     </div>
+
+    </BrowserRouter>
   );
 }
 
